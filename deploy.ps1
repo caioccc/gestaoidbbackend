e78f64c89d756691a38f74f86f938eaa7187f269
Caio Marinho <caiomarinho8@gmail.com>
2026-09-18 11:48:04 -0300

    insert script

--- deploy.ps1
+++ deploy.ps1
@@ -1,15 +1,18 @@
-Write-Host "1. Limpando pycache local..." -ForegroundColor Cyan
+Write-Host "1. Autenticando Docker no Heroku..." -ForegroundColor Cyan
+heroku container:login
+
+Write-Host "2. Limpando pycache local..." -ForegroundColor Cyan
 Get-ChildItem -Path . -Recurse -Include __pycache__,*.pyc | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "2. Compilando imagem Docker..." -ForegroundColor Cyan
+Write-Host "3. Compilando imagem Docker..." -ForegroundColor Cyan
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-docker build --no-cache --build-arg TIMESTAMP=$timestamp -t registry.heroku.com/gestaoidb/web .
+docker build --no-cache --build-arg TIMESTAMP=$timestamp -t registry.heroku.com/gestaoidb/web:latest .
 
-Write-Host "3. Enviando para o Heroku..." -ForegroundColor Cyan
-docker push registry.heroku.com/gestaoidb/web
+Write-Host "4. Enviando imagem para o Heroku..." -ForegroundColor Cyan
+docker push registry.heroku.com/gestaoidb/web:latest
 
-Write-Host "4. Aplicando Release..." -ForegroundColor Cyan
-.\script_deploy.ps1
+Write-Host "5. Aplicando Release..." -ForegroundColor Cyan
+heroku container:release web -a gestaoidb
 
-Write-Host "5. Verificando versao..." -ForegroundColor Cyan
+Write-Host "6. Verificando versao..." -ForegroundColor Cyan
 heroku releases -a gestaoidb -n 3
